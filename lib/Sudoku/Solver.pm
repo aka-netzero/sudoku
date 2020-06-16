9a5387ed939fa0531e4aba04c9a137bb7de5ddb0
aka-netzero <steve.aurigema@gmail.com>
2020-06-16 12:06:40 +0200

    A tiny bit of documentation for ->new
    Added the ability to silence per-run stats

--- lib/Sudoku/Solver.pm
+++ lib/Sudoku/Solver.pm
@@ -4,6 +4,7 @@ use v5.30;
 use feature 'signatures';
 no warnings 'experimental::signatures';
 
+use sayf;
 use Time::HiRes qw(time);
 use Data::Dumper;
 
@@ -15,6 +16,15 @@ use constant SUPPORTED_TYPES => {
     )
 };
 
+
+
+# Sudoku::Solver->new({
+#   board_string => ...
+#           type => one of keys %{SUPPORTED_TYPES}, maps to package in Sudoku::Solver::
+#                   namespace
+#       arguments=> hashref of arguments to send to the solver
+#           debug=> any truthy value will include additional information upon failure
+# })
 sub new ( $class, $options ) {
     die 'You must provide a board string in the board argument to Solver->new!' unless defined $options->{board_string};
 
@@ -27,16 +37,23 @@ sub new ( $class, $options ) {
 
     my $self = {
         solver => ${solver_package}->new( $options->{arguments} ),
+        _debug => !!$options->{debug},
     };
 
     return bless $self, $class;
 }
 
 sub solver ( $self ) { return $self->{solver}; }
+sub debug  ( $self ) { return $self->{_debug}; }
 
-sub solve_n_times ( $self, $n ) {
+sub solve_n_times ( $self, $n, $print_per_run_info = 1 ) {
     # runs = ( [iterations,time_spent], ... )
     my @runs;
+    my ($fastest,$slowest) = (99999999,0);
+    my $extremes = {
+        fastest => [],
+        slowest => [],
+    };
 
     for ( 1..$n ) {
         my $START_TIME = time;
@@ -44,17 +61,42 @@ sub solve_n_times ( $self, $n ) {
         my $runtime    = sprintf '%.3f', ( (time-$START_TIME) * 1000);
 
         if ( $iterations ) {
-            say "Run $_ completed in ${runtime}ms after ${iterations} steps";
+            if ( $runtime > $slowest ) {
+                $slowest = $runtime;
+                $extremes->{slowest} = [ $iterations,$runtime ];
+            } elsif ( $runtime < $fastest ) {
+                $fastest = $runtime;
+                $extremes->{fastest} = [ $iterations,$runtime ];
+            }
+            if ( $print_per_run_info ) {
+                say "Run $_ completed in ${runtime}ms after ${iterations} steps";
+            }
         } else {
-            say "Run $_ failed for some reason.. should probably debug this one. Here's a dumper of the object:" . Dumper $self->solver;
-            say "I've paused execution, you can either CTRL+C or hit enter to continue.";
-            <STDIN>;
+            if ( $self->debug ) {
+                say Dumper($self->solver);
+            }
+            sayf "Run %d failed in %.3fms. Is it possible this board is unsolveable? Above you can see a Dumper of the solver object.",$_, $runtime;
+            if ( $self->debug ) {
+                say "I've paused execution, you can either CTRL+C or hit enter to continue.";
+                <STDIN>;
+            }
         }
 
         $self->solver->reset;
 
         push @runs, [ $iterations, $runtime ];
     }
+    eval {
+        sayf "Completed %d solves of the provided board.", $n;
+        sayf "The fastest solve completed in %.3fms and %d iterations.", $extremes->{fastest}[1], $extremes->{fastest}[0];
+        sayf "The slowest solve completed in %.3fms and %d iterations.", $extremes->{slowest}[1], $extremes->{slowest}[0];
+        sayf "Making the fastest one %.02f%% faster than the slowest.", ($extremes->{slowest}[1] / $extremes->{fastest}[1]) * 100;
+        1;
+    } or do {
+        say Dumper $extremes;
+    };
 }
+
+# 56.683 / 131.455
 
 1;
