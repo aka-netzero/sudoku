c63f7cca40a05dbb21076e0ce64a0dd5af739740
aka-netzero <steve.aurigema@gmail.com>
2020-06-18 12:02:20 +0200

    It can partially solve quite quickly, but something is still wrong..

--- bits.pl
+++ bits.pl
@@ -5,40 +5,131 @@ no warnings 'experimental::signatures';
 use lib './lib/';
 use sayf;
 
+use Time::Hires qw( time );
+
 use constant {
-    ONE     => 1 << 0,  SIX     => 1 << 5,
-    TWO     => 1 << 1,  SEVEN   => 1 << 6,
-    THREE   => 1 << 2,  EIGHT   => 1 << 7,
-    FOUR    => 1 << 3,  NINE    => 1 << 8,
-    FIVE    => 1 << 4,
+    ZERO    => 1 << 0,  FIVE    => 1 << 5,
+    ONE     => 1 << 1,  SIX     => 1 << 6,
+    TWO     => 1 << 2,  SEVEN   => 1 << 7,
+    THREE   => 1 << 3,  EIGHT   => 1 << 8,
+    FOUR    => 1 << 4,  NINE    => 1 << 9,
+
 };
 
 use constant DEC_TO_BIN => {
+    0 => ZERO,      5 => FIVE,
     1 => ONE,       6 => SIX,
     2 => TWO,       7 => SEVEN,
     3 => THREE,     8 => EIGHT,
     4 => FOUR,      9 => NINE,
-    5 => FIVE,
 };
 
-sub sayb ( $binary ) { sayf( "%#012b %1\$3d", $binary ); }
-
+use constant BIN_TO_DEC => {
+    ZERO()    => 0,   FIVE()    => 5,
+    ONE()     => 1,   SIX()     => 6,
+    TWO()     => 2,   SEVEN()   => 7,
+    THREE()   => 3,   EIGHT()   => 8,
+    FOUR()    => 4,   NINE()    => 9,
+};
 
 my $board_string = "040020900000000010000006850582300700000807000009005138097100000020000000004030000";
+
+my $MISSING_CELLS   = () = $board_string =~ /0/g;
+my $ITERATION_COUNT = 0;
+
 my ($board,$rows,$columns,$squares) = convert_board_string ( $board_string );
 
-get_possible_values(1,$rows,$columns,$squares);
+my $START_TIME = time;
+my ($last_index,$completed_board,$r,$c,$s) = solve(
+    next_position($board,$rows,$columns,$squares),
+    $board,     $rows,
+    $columns,   $squares,
+);
+my $END_TIME = time;
 
+if ( defined $last_index ) {
+    sayf "I think we solved it in %d iterations, and a total of %sms", $ITERATION_COUNT, ($END_TIME - $START_TIME) * 1000;
+    print_board( $completed_board );
+} else {
+    say "Something went wrong..";
+}
 exit;
 
-# index, rows, columns, squares
-sub get_possible_values ( $i,$r,$c,$s ) {
-    my $possibilities = ~( $r->[ int($i/9) ] | $c->[$i%9] | $squares->[ int( int( $i / 27 ) * 3 + ( $i % 9 / 3 ) ) ] );
-    for ( 1..9 ) {
-        unless ( $possibilities & DEC_TO_BIN->{$_} ) {
-            sayf "Number %d is available!", $_;
+sub solve ( $index, $board, $rows, $columns, $squares, $correct_count = 0 ) {
+    ++$ITERATION_COUNT;
+    if ( $correct_count == $MISSING_CELLS ) {
+        say "Solved?";
+        return ($index,$board,$rows,$columns,$squares,$correct_count);
+    }
+
+    my @possible_values = get_possible_values($board,$index,$rows,$columns,$squares);
+
+    if ( @possible_values == 1 && $correct_count + 1 == $MISSING_CELLS ) {
+        say "At the last possible cell ( possible_values == 1 and correct_count + 1 == missing_cells";
+        $board->[$index] = shift @possible_values;
+        return ($index,$board,$rows,$columns,$squares,$MISSING_CELLS);
+    }
+
+    my $row_index    = int($index/9);
+    my $col_index    = $index % 9;
+    my $square_index = int( int( $index / 27 ) * 3 + ( $index % 9 / 3 ) );
+
+    sayf "Selecting a possible value out of %d possibilities", scalar(@possible_values);
+    while ( my $try_value = splice @possible_values, int(rand()*scalar(@possible_values)), 1 ) {
+        my $new_board = [@$board];      my $r = [@$rows];
+        my $c         = [@$columns];    my $s = [@$squares];
+        my $cc        = $correct_count; my $i;
+        $new_board->[$index] = $try_value;
+        $r->[$row_index]    |= $try_value;
+        $c->[$col_index]    |= $try_value;
+        $s->[$square_index] |= $try_value;
+        $i = next_position($new_board,$r,$c,$s);
+
+        ($i,$b,$r,$c,$s,$cc) = solve($i,$new_board,$r,$c,$s,$cc + 1);
+
+        if ( $cc == $MISSING_CELLS ) {
+            return ($i,$b,$r,$c,$s,$cc);
         }
     }
+
+    return ();
+}
+
+sub next_position ( $board, $r, $c, $s ) {
+    # say "next_position called";
+    my @empty_indexes = grep { $board->[$_] == ZERO } 0..$#{$board};
+    # sayf "\t%d total empty indexes found", scalar(@empty_indexes);
+    my %indexes_by_number_of_options;
+    my $lowest_count = 9;
+
+    for my $idx ( @empty_indexes ) {
+        my $possibility_count = scalar( get_possible_values($board,$idx,$r,$c,$s) );
+        # sayf "\tindex %d has %d possible valid values", $idx, $possibility_count;
+
+        if ( $possibility_count < $lowest_count ) {
+            $lowest_count = $possibility_count;
+            push @{$indexes_by_number_of_options{$possibility_count} //= [] }, $idx;
+        }
+    }
+    my $selected_index = $indexes_by_number_of_options{$lowest_count}->[ int( rand() * scalar(@{$indexes_by_number_of_options{$lowest_count}}) ) ];
+    sayf "Selected %d as the next_position", $selected_index;
+    return $selected_index;
+}
+
+sub get_possible_values ( $board, $index, $rows, $columns, $squares ) {
+    my @possible_values;
+    my $row_index    = int($index/9);
+    my $col_index    = $index % 9;
+    my $square_index = int( int( $index / 27 ) * 3 + ( $index % 9 / 3 ) );
+    my $possibilities = ~( $rows->[$row_index] | $columns->[$col_index] | $squares->[$square_index] );
+
+    for my $value ( 1..9 ) {
+        unless ( $possibilities & DEC_TO_BIN->{$value} ) {
+            push @possible_values, DEC_TO_BIN->{$value};
+        }
+    }
+    
+    return @possible_values;
 }
 
 sub convert_board_string ( $board_string ) {
@@ -54,6 +145,17 @@ sub convert_board_string ( $board_string ) {
         $squares[$square]|= $value; 
     }
     return (\@board,\@rows,\@columns,\@squares);
+}
+
+sub print_board ( $board, $highlight_index = undef ) {
+    for my $index ( 0..80 ) {
+        printf $highlight_index && $highlight_index == $index ? '[%s] ' : '%s ', BIN_TO_DEC->{$board->[$index]};
+        if ( $index > 0 && ($index + 1) % 3 == 0 ) {
+            print ' ';
+        }
+        print "\n" if $index && ($index + 1) % 9 == 0;
+        print "\n" if $index && ($index + 1) % 27 == 0;
+    }
 }
 
 __END__
