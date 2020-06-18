f5930b26ac6c9b343025aa9c0f681a40f1cc8a05
aka-netzero <steve.aurigema@gmail.com>
2020-06-18 10:39:49 +0200

    Ok now it's starting to shape up a bit

--- bits.pl
+++ bits.pl
@@ -21,35 +21,24 @@ use constant DEC_TO_BIN => {
     5 => FIVE,
 };
 
-# Indicates that 4 and 2 are already used
-my $values = TWO | FOUR;
-
-sayb( $values );
-for ( 1..9 ) {
-    unless ( $values & DEC_TO_BIN->{$_} ) {
-        sayf "Number %d is available!", $_;
-    }
-}
-exit;
-
-
 sub sayb ( $binary ) { sayf( "%#012b %1\$3d", $binary ); }
-
-exit;
-
-# for ( my $k = 1; $k<40; $k += 7 ) {
-#     sayf "%10b %3d",$k,$k;
-# }
 
 
 my $board_string = "040020900000000010000006850582300700000807000009005138097100000020000000004030000";
 my ($board,$rows,$columns,$squares) = convert_board_string ( $board_string );
 
-sub get_possible_values ( $index ) {
-    my $value = $board->[$index];
-    my @possible_values = (
+get_possible_values(1,$rows,$columns,$squares);
 
-    )
+exit;
+
+# index, rows, columns, squares
+sub get_possible_values ( $i,$r,$c,$s ) {
+    my $possibilities = ~( $r->[ int($i/9) ] | $c->[$i%9] | $squares->[ int( int( $i / 27 ) * 3 + ( $i % 9 / 3 ) ) ] );
+    for ( 1..9 ) {
+        unless ( $possibilities & DEC_TO_BIN->{$_} ) {
+            sayf "Number %d is available!", $_;
+        }
+    }
 }
 
 sub convert_board_string ( $board_string ) {
@@ -88,27 +77,6 @@ sub get_xy_from_flat ( $index ) {
 0 9 7   1 0 0   0 0 0
 0 2 0   0 0 0   0 0 0
 0 0 4   0 3 0   0 0 0
-
-
-board[1]     = 0b000001000
-rows[0]     |= 0b000001000 = (0b000000000 | 0b000001000) = 0b000001000
-columns[1]  |= 0b000001000 = (0b000000000 | 0b000001000) = 0b000001000
-square[0]   |= 0b000001000 = (0b000000000 | 0b000001000) = 0b000001000
-
-board[4]     = 0b000000010
-rows[0]     |= 0b000000010 = (0b000001000 | 0b000000010) = 0b0000010010
-columns[4]  |= 0b000000010
-square[1]   |= 0b000000010
-1 0b0000000010   2
-2 0b0000000100   4
-3 0b0000001000   8
-4 0b0000010000  16
-5 0b0000100000  32
-6 0b0001000000  64
-7 0b0010000000 128
-8 0b0100000000 256
-9 0b1000000000 512
-
 
 X = {1, 2, 3, 4, 5, 6, 7}
 Y = {
