268e705d3cd9ec602c673c3d99bb73e15b380144
aka-netzero <steve.aurigema@gmail.com>
2020-07-07 10:33:01 +0200

    Backported the improvements made in my cpp version to this one, looks like it shaved ~0.3ms off of the best solve time with this algorithm.

--- lib/Sudoku/Solver/FlatArrayBacktrackV2.pm
+++ lib/Sudoku/Solver/FlatArrayBacktrackV2.pm
@@ -12,7 +12,6 @@ use constant {
     TWO     => 1 << 2,  SEVEN   => 1 << 7,
     THREE   => 1 << 3,  EIGHT   => 1 << 8,
     FOUR    => 1 << 4,  NINE    => 1 << 9,
-
 };
 
 use constant DEC_TO_BIN => {
@@ -24,46 +23,57 @@ use constant DEC_TO_BIN => {
 };
 
 use constant BIN_TO_DEC => {
-    ZERO()    => 0,   FIVE()    => 5,
-    ONE()     => 1,   SIX()     => 6,
-    TWO()     => 2,   SEVEN()   => 7,
-    THREE()   => 3,   EIGHT()   => 8,
-    FOUR()    => 4,   NINE()    => 9,
+      ZERO() => 0,  FIVE() => 5,
+       ONE() => 1,   SIX() => 6,
+       TWO() => 2, SEVEN() => 7,
+     THREE() => 3, EIGHT() => 8,
+      FOUR() => 4,  NINE() => 9
 };
 
 sub new ( $class, $options ) {
-    my ($board,$rows,$columns,$squares) = convert_board_string ( $options->{board_string} );
     my $self = {
-        _board   => $board,
-        _rows    => $rows,
-        _columns => $columns,
-        _squares => $squares,
+        _board   => [],
+        _rows    => [],
+        _columns => [],
+        _squares => [],
         
+        _board_string => $options->{board_string},
         _iterations => 0,
     };
-    $self->{_cells_needed_to_win} = () = $options->{board_string} =~ /0/g;
-    $self->{_originals} = {
-        _board   => [ @{ $self->{_board}   } ],
-        _rows    => [ @{ $self->{_rows}    } ],
-        _columns => [ @{ $self->{_columns} } ],
-        _squares => [ @{ $self->{_squares} } ],
-    };
 
-    return bless $self, __PACKAGE__;
+    $self->{_cells_needed_to_win} = () = $options->{board_string} =~ /0/g;
+
+    $self = bless $self, __PACKAGE__;
+
+    $self->fill_structures;
+    return $self;
+}
+
+sub fill_structures ( $self ) {
+    my @cells = split //, $self->{_board_string};
+    $self->{$_} = [] for qw( _board _rows _columns _squares );
+
+    for ( 0 .. 80 ) {
+        my $square = int( int( $_ / 27 ) * 3 + ( $_ % 9 / 3 ) );
+        my $value = DEC_TO_BIN->{$cells[$_]};
+
+        $self->{_board}[$_] = $value;
+        $self->{_rows}[int($_/9)] |= $value;
+        $self->{_columns}[$_%9]   |= $value;
+        $self->{_squares}[$square]|= $value;
+    }
 }
 
 sub solve ( $self ) {
     my @necessary_args = ($self->{_board},$self->{_rows}, $self->{_columns}, $self->{_squares});
     my $start_position = next_position(@necessary_args);
-    my ($last_index,$last_board,$r,$c,$s,$filled_cells) = $self->_solve($start_position, @necessary_args);
 
-    if ( $last_board && $filled_cells ) {
+    my $filled_cells = $self->_solve($start_position, @necessary_args);
+
+    if ( $filled_cells ) {
         if ( $filled_cells == $self->{_cells_needed_to_win} ) {
             return $self->{_iterations};
         } else {
-            say "Well now that's odd isn't it? 'Solved' board looks like: ";
-            print_board($last_board);
-            printf "But the correct count does not match the expected: %s\n", $filled_cells // 'undef';
             return undef;
         }
     }
@@ -73,7 +83,7 @@ sub solve ( $self ) {
 sub _solve ( $self, $index, $board, $rows, $columns, $squares, $correct_count = 0 ) {
     ++$self->{_iterations};
     if ( $correct_count == $self->{_cells_needed_to_win} ) {
-        return ($index,$board,$rows,$columns,$squares,$correct_count);
+        return $correct_count;
     }
 
     my @possible_values = get_possible_values($board,$index,$rows,$columns,$squares);
@@ -82,7 +92,7 @@ sub _solve ( $self, $index, $board, $rows, $columns, $squares, $correct_count = 
     # Why waste the ops for the rest?
     if ( @possible_values == 1 && $correct_count + 1 == $self->{_cells_needed_to_win} ) {
         $board->[$index] = shift @possible_values;
-        return ($index,$board,$rows,$columns,$squares,$self->{_cells_needed_to_win});
+        return $self->{_cells_needed_to_win};
     }
 
     my $row_index    = int($index/9);
@@ -90,19 +100,22 @@ sub _solve ( $self, $index, $board, $rows, $columns, $squares, $correct_count = 
     my $square_index = int( int( $index / 27 ) * 3 + ( $index % 9 / 3 ) );
 
     while ( my ($try_value) = splice @possible_values, int(rand()*scalar(@possible_values)), 1 ) {
-        my $new_board = [@$board];      my $r = [@$rows];
-        my $c         = [@$columns];    my $s = [@$squares];
-        my $cc        = $correct_count; my $i;
-        $new_board->[$index] = $try_value;
-        $r->[$row_index]    |= $try_value;
-        $c->[$col_index]    |= $try_value;
-        $s->[$square_index] |= $try_value;
-        $i = next_position($new_board,$r,$c,$s);
+        my $i;
+        $board->[$index]           = $try_value;
+        $rows->[$row_index]       |= $try_value;
+        $columns->[$col_index]    |= $try_value;
+        $squares->[$square_index] |= $try_value;
+        $i = next_position($board,$rows,$columns,$squares);
 
-        ($i,$b,$r,$c,$s,$cc) = $self->_solve($i,$new_board,$r,$c,$s,$cc + 1);
+        my $cc = $self->_solve($i,$board,$rows,$columns,$squares,$correct_count + 1);
 
         if ( $cc == $self->{_cells_needed_to_win} ) {
-            return ($i,$b,$r,$c,$s,$cc);
+            return $cc;
+        } else {
+            $board->[$index] = ZERO;
+            $rows->[$row_index]       ^= $try_value;
+            $columns->[$col_index]    ^= $try_value;
+            $squares->[$square_index] ^= $try_value;
         }
     }
 
@@ -110,21 +123,22 @@ sub _solve ( $self, $index, $board, $rows, $columns, $squares, $correct_count = 
 }
 
 sub reset ( $self ) {
-    $self->{$_} = $self->{_originals}{$_} for keys %{ $self->{_originals} };
+    $self->fill_structures;
     $self->{_iterations} = 0;
 }
 
 sub next_position ( $board, $r, $c, $s ) {
-    my @empty_indexes = grep { $board->[$_] == ZERO } 0..$#{$board};
     my %indexes_by_number_of_options;
     my $lowest_count = 9;
 
-    for my $idx ( @empty_indexes ) {
-        my $possibility_count = scalar( get_possible_values($board,$idx,$r,$c,$s) );
+    for my $idx ( 0..$#{$board} ) {
+        if ( $board->[$idx] == ZERO ) {
+            my $possibility_count = scalar( get_possible_values($board,$idx,$r,$c,$s) );
 
-        if ( $possibility_count < $lowest_count ) {
-            $lowest_count = $possibility_count;
-            push @{$indexes_by_number_of_options{$possibility_count} //= [] }, $idx;
+            if ( $possibility_count < $lowest_count ) {
+                $lowest_count = $possibility_count;
+                push @{$indexes_by_number_of_options{$possibility_count} //= [] }, $idx;
+            }
         }
     }
 
@@ -147,19 +161,15 @@ sub get_possible_values ( $board, $index, $rows, $columns, $squares ) {
     return @possible_values;
 }
 
-sub convert_board_string ( $board_string ) {
-    my @cells = split //, $board_string;
-    my (@board,@rows,@columns,@squares);
-
-    for ( 0 .. 80 ) {
-        my $square = int( int( $_ / 27 ) * 3 + ( $_ % 9 / 3 ) );
-        my $value = DEC_TO_BIN->{$cells[$_]};
-        $board[$_] = $value;
-        $rows[int($_/9)] |= $value;
-        $columns[$_%9]   |= $value;
-        $squares[$square]|= $value; 
+sub print_board ( $board, $highlight_index = undef ) {
+    for my $index ( 0..80 ) {
+        printf $highlight_index && $highlight_index == $index ? '[%s] ' : '%s ', BIN_TO_DEC->{$board->[$index]} // '?';
+        if ( $index > 0 && ($index + 1) % 3 == 0 ) {
+            print ' ';
+        }
+        print "\n" if $index && ($index + 1) % 9 == 0;
+        print "\n" if $index && ($index + 1) % 27 == 0;
     }
-    return (\@board,\@rows,\@columns,\@squares);
 }
 
 1;
